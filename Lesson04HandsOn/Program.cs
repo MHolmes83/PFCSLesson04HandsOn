550d4dbda80ff2cc702d44bc049c22f78f276ef9
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2023-11-21 10:34:47 -0500

    Lesson04HandsOn
    Created a simple dotnet project and created three methods--one that doubled a given number, another that used the function of the first to quadruple a given number, and a third doubled a number multiple times at once--then ran all three methods through the Main methods.

---
+++ Lesson04HandsOn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson04HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doubled Number: " + DoubleMe(4));
+
+            Console.WriteLine("Quadrupled Number: " + QuadrupleMe(5));
+
+            Console.WriteLine("Number Doubled Many Times: " + DoubleNumberMultipleTimes(2, 3));
+        }
+
+        public static int DoubleMe(int number)
+        {
+            return(number * 2);
+        }
+
+        public static int QuadrupleMe(int number)
+        {
+            return(DoubleMe(DoubleMe(number)));
+        }
+
+        public static int DoubleNumberMultipleTimes(int numberToBeDoubled, int amountOfTimes)
+        {
+            for (int i = 0; i < amountOfTimes; i++)
+            {
+                numberToBeDoubled = DoubleMe(numberToBeDoubled);
+            }
+
+            return numberToBeDoubled;
+        }
+    }
+}
